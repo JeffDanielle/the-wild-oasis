74c5183082544a23395926f4e9265c711af44800
JeffDanielle <bayotjeff@gmail.com>
2024-04-10 16:30:59 +0800

    352. Displaying Toasts(Notifications) -11min

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { deleteCabin } from "../../services/apiCabins";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -46,14 +47,14 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin successfully deleted')
-      // invalidateQueries refetch data
+      toast.success('Cabin successfully deleted')
+      // invalidateQueries refetch data which will render instantly in the component
       queryClient.invalidateQueries({
         queryKey: ['cabins']
       });
     },
     onError: (err) => {
-      alert(err.message)
+      toast.error(err.message)
     }
   })
 
