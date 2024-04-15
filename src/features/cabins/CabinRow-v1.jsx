579d69f6749029199192dabce956f45f77c8c32e
JeffDanielle <bayotjeff@gmail.com>
2024-04-15 09:13:09 +0800

    374. Building a Reusable Context Menu -33min

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -11,7 +11,6 @@ import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
 
 
 // const TableRow = styled.div`
@@ -77,38 +76,24 @@ function CabinRow({ cabin }) {
       <Price>{formatCurrency(regularPrice)}</Price>
       {discount ? <Discount>{formatCurrency(discount)}% off</Discount> : <span>&mdash;</span>}
       <div>
+        <button disabled={isCreating} onClick={handleDuplicate}><HiSquare3Stack3D /></button>
+
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId} />
-
-
-
-            <Menus.List id={cabinId}>
-              <Menus.Button icon={<HiSquare3Stack3D />} onClick={handleDuplicate}>Duplicate</Menus.Button>
-
-              <Modal.Open opens="edit">
-                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              </Modal.Open>
-
-              <Modal.Open opens="delete">
-                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-              </Modal.Open>
-
-
-            </Menus.List>
-
-            <Modal.Window name="edit">
-              <CreateCabinForm cabinToEdit={cabin} />
-            </Modal.Window>
-
-
-            <Modal.Window name="delete">
-              <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)} />
-            </Modal.Window>
-          </Menus.Menu>
+          <Modal.Open opens="edit">
+            <button><HiPencil /></button>
+          </Modal.Open>
+          <Modal.Window name="edit">
+            <CreateCabinForm cabinToEdit={cabin} />
+          </Modal.Window>
+          <Modal.Open opens="delete">
+            <button ><HiTrash /></button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)} />
+          </Modal.Window>
         </Modal>
       </div>
-    </Table.Row >
+    </Table.Row>
   )
 }
 
