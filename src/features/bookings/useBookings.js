124adec8fc2d5c55f07e9a3cd3ff34421deb02b8
JeffDanielle <bayotjeff@gmail.com>
2024-04-15 18:14:09 +0800

    381. API-Side Sorting: Sorting Bookings -7min

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -11,10 +11,15 @@ export function useBookings() {
         { field: 'status', value: filterValue }
     // { field: 'totalPrice', value: 5000, method: 'gte' }
 
+    // SORT
+    const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+    const [field, direction] = sortByRaw.split('-');
+    const sortBy = { field, direction };
+
     const { isLoading, data: bookings, error } = useQuery({
         // Whenever a change of value in filter it will refetch data
-        queryKey: ["bookings", filter],
-        queryFn: () => getBookings({ filter }),
+        queryKey: ["bookings", filter, sortBy],
+        queryFn: () => getBookings({ filter, sortBy }),
     })
 
     return { isLoading, bookings, error }
