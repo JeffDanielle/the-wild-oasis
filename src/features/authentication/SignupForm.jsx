3cea61b2a7737ff073479499208604fb409330e7
JeffDanielle <bayotjeff@gmail.com>
2024-04-17 14:47:04 +0800

    395. User Sign Up -15min

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,25 +3,31 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm()
+  const { register, formState, getValues, handleSubmit, reset } = useForm()
   const { errors } = formState
+  const { signup, isLoading } = useSignup()
 
-  function onSubmit(data) {
-    console.log(data)
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password },
+      {
+        onSettled: () => reset(),
+      }
+    );
   }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input type="text" id="fullName" {...register("fullName", { required: "This field is required" })} />
+        <Input type="text" id="fullName" disabled={isLoading} {...register("fullName", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow label="Email address" error={errors?.email?.message}>
-        <Input type="email" id="email" {...register("email", {
+        <Input type="email" id="email" disabled={isLoading} {...register("email", {
           required: "This field is required",
           pattern: {
             value: /\S+@\S+\.\S+/,
@@ -31,7 +37,7 @@ function SignupForm() {
       </FormRow>
 
       <FormRow label="Password (min 8 characters)" error={errors?.password?.message}>
-        <Input type="password" id="password" {...register("password", {
+        <Input type="password" id="password" disabled={isLoading} {...register("password", {
           required: "This field is required",
           minLength: {
             value: 8,
@@ -41,7 +47,7 @@ function SignupForm() {
       </FormRow>
 
       <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
-        <Input type="password" id="passwordConfirm" {...register("passwordConfirm", {
+        <Input type="password" id="passwordConfirm" disabled={isLoading} {...register("passwordConfirm", {
           required: "This field is required",
           validate: (value) => value === getValues().password || "Passwords need to match",
         })} />
@@ -49,10 +55,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
